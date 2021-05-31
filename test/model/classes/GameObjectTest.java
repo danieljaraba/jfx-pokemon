0bf5e28fd2e97abb754f37e05d9a500337afd0e2
CamiloCJ09 <camilocampazj@gmail.com>
2021-05-30 21:45:00 -0500

    Adding docs

---
+++ test/model/classes/GameObjectTest.java
@@ -0,0 +1,14 @@
+package model.classes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameObjectTest {
+
+    private Village village;
+
+    @Test
+    void isNear() {
+    }
+}

aa5333d3a7fbbd6fa220aec921b6d047df5dfcd0
CamiloCJ09 <camilocampazj@gmail.com>
2021-06-11 01:57:23 -0500

    Adding more tests and javadoc

--- test/model/classes/BagTest.java
+++ test/model/classes/BagTest.java
@@ -1,7 +1,12 @@
 package model.classes;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BagTest {
 
+    @Test
+    void sortUsedPokeballsByPokemonName() {
+    }
 }
